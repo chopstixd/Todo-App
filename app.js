28ec77ffc6d6b96debcb1f591af1cebd4c5ae50a
Gospel Jonathan <gospyjo@gmail.com>
2025-03-04 23:24:37 +0100

    fixed todo

--- app.js
+++ app.js
@@ -1,79 +1,84 @@
-const todoForm = document.querySelector("form");
+const todoForm = document.getElementById("todo-form");
 const todoInput = document.getElementById("todo-input");
 const todoListUL = document.getElementById("todo-list");
 
 let allTodos = getTodos();
 updateTodoList();
 
-todoForm.addEventListener("submi", function (e) {
+todoForm.addEventListener("submit", function (e) {
   e.preventDefault();
+  addTodo();
 });
 
 function addTodo() {
   const todoText = todoInput.value.trim();
-  if (todoListUL.length > 0) {
-    const todoObject = {
-      text: todoText,
-      completed: false,
-    };
-    allTodos.push(todoObject);
-    updateTodoList();
-    saveTodos();
-    todoInput.value = "";
-  }
+
+  if (todoText === "") return; // Prevent adding empty todos
+
+  const todoObject = {
+    text: todoText,
+    completed: false,
+  };
+
+  allTodos.push(todoObject);
+  updateTodoList();
+  saveTodos();
+  todoInput.value = "";
 }
 
 function updateTodoList() {
   todoListUL.innerHTML = "";
   allTodos.forEach((todo, todoIndex) => {
-    todoListUL.append(todoItem);
+    const todoItem = createTodoItem(todo, todoIndex);
+    todoListUL.appendChild(todoItem);
   });
 }
 
 function createTodoItem(todo, todoIndex) {
   const todoId = "todo-" + todoIndex;
   const todoLI = document.createElement("li");
-  const todoText = todo.text;
   todoLI.className = "todo";
   todoLI.innerHTML = ` 
-             <input type="checkbox" id="${todoId}">
-            <label class="custom-checkbox" for="todo-1"></label>
-            <link
-              rel="stylesheet"
-              href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=check"
-            />
-            <label for="${todoId}" class="todo-text">
-              ${todoText}
-            </label>
-            <button class="delete-button">
-              <img
-                src="./material-symbols-light_delete-outline-sharp.png"
-                alt="delete-icon"
-              />
-            </button>
-            `;
+    <input type="checkbox" id="${todoId}">
+    <label class="custom-checkbox" for="${todoId}"></label>
+    <label for="${todoId}" class="todo-text">
+      ${todo.text}
+    </label>
+    <button class="delete-button">
+      <img
+        src="./material-symbols-light_delete-outline-sharp.png"
+        alt="delete-icon"
+      />
+    </button>
+  `;
+
   const deleteButton = todoLI.querySelector(".delete-button");
   deleteButton.addEventListener("click", () => {
     deleteTodoItem(todoIndex);
   });
-  const checkbox = todoLI.querySelector("input");
+
+  const checkbox = todoLI.querySelector("input[type='checkbox']");
   checkbox.addEventListener("change", () => {
     allTodos[todoIndex].completed = checkbox.checked;
     saveTodos();
   });
   checkbox.checked = todo.completed;
+
   return todoLI;
 }
+
 function deleteTodoItem(todoIndex) {
   allTodos = allTodos.filter((_, i) => i !== todoIndex);
   saveTodos();
   updateTodoList();
 }
+
 function saveTodos() {
   const todosJson = JSON.stringify(allTodos);
   localStorage.setItem("todos", todosJson);
 }
+
 function getTodos() {
-  const todos = localStorage.getItem("todos") || "[]";
-  return JSON.parse(todos);
+  const todos = localStorage.getItem("todos");
+  return todos ? JSON.parse(todos) : [];
 }
